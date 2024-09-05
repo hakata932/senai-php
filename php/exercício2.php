f1c0ad966dc3cf3be738d3ae11f86e6255348e55
Hakata932 <carlos.emanuel932@gmail.com>
2024-09-05 08:23:28 -0500

    loop de 0 a 50

--- php/exercício2.php
+++ php/exercício2.php
@@ -1,14 +1,21 @@
 <?php 
 //este código realiza uma verificação da idade que for inserida, e novamente, ainda não é feito com o input do usuário
 
-$idade = 40;
-if($idade >= 0 && $idade <= 10){
-    echo "esta pessoa é uma criança";
-} else if($idade > 10 && $idade <= 19){
-    echo "esta pessoa é um adolescente";
-} else if($idade > 19 && $idade <= 35){
-    echo "esta pessoa é um adulto jovem";
-} else{
-    echo "esta pessoa é uma adulta";
+//$idade = 40;
+//if($idade >= 0 && $idade <= 10){
+//    echo "esta pessoa é uma criança";
+//} else if($idade > 10 && $idade <= 19){
+//    echo "esta pessoa é um adolescente";
+//} else if($idade > 19 && $idade <= 35){
+//    echo "esta pessoa é um adulto jovem";
+//} else{
+//    echo "esta pessoa é uma adulta";
+//}
+//
+$contador = 0;
+
+while($contador != 50){
+    $contador = $contador + 2;
 }
+echo $contador;
 ?>
