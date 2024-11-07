50d32e6424a1663e5928baae21b85832e56722db
Hakata932 <carlos.emanuel932@gmail.com>
2024-11-07 13:43:10 -0300

    Update cadastrados.php

--- treino saep/cadastrados.php
+++ treino saep/cadastrados.php
@@ -13,7 +13,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
